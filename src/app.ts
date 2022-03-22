88ee544c288166f995113fd0c7c6b06a0d54158a
elninocavalcanti10 <acn2011@outlook.com>
2022-03-22 14:52:52 -0300

    Iniciando a Api

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bora AWS 002!'));
+app.get('/', (req: any, res: any) => res.json({
+    nome: "Adolpho",
+    cargo: "Engenheiro de Software"
+}));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
